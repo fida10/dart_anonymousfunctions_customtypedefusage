130e90ef9579a86f938c68c7c0409123126f09f3
eshad <fidaeshad10@gmail.com>
2023-11-26 04:23:55 -0600

    first commit

---
+++ example/anonymousfunctions_customtypedefusage_example.dart
@@ -0,0 +1,6 @@
+import 'package:anonymousfunctions_customtypedefusage/anonymousfunctions_customtypedefusage.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
